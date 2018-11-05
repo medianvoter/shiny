5978465777bd5ab0504beba9800a384c6aaef2e5
medianvoter <garbagemailclock@gmail.com>
2018-11-05 23:45:49 +0800

    update

--- server.R
+++ server.R
@@ -1,12 +1,11 @@
 # Developing Data Products Shiny Project
 
-#' In server.R, I perform the random draws from the stated distribution
-#' (assuming) the parameters like mu and sigma (normal), min and max (for unif),
-#' and lambda (for poisson and exponential).
-#' 
-#' It plots two graphs.
-#' 
-#' Deployed! https://medvoter.shinyapps.io/shiny/
+# In server.R, I perform the random draws from the stated distribution
+# (assuming) the parameters like mu and sigma (normal), min and max (for unif),
+# and lambda (for poisson and exponential).
+# 
+# It plots two graphs and allows the user to compare the distributions, see the
+# Central Limit Theorem for themselves.
 
 library(shiny)
 
@@ -17,9 +16,13 @@ shinyServer(function(input, output) {
         require(ggplot2)
         set.seed(8675309)
         
+        # Poisson distribution doesn't have default lambda, checks if the input
+        # radio button (called dist) is for the Poisson distribution, and puts
+        # in lambda
         if(as.character(input$dist) == "pois") {
             draws <- rpois(input$numSamples, lambda = 1)
         }
+        # Other distributions have defaults
         else {
             dist <- switch(input$dist, norm = rnorm, unif = runif, pois = rpois,
                            exp = rexp, rnorm)
@@ -35,12 +38,16 @@ shinyServer(function(input, output) {
         require(ggplot2)
         set.seed(8675309)
         
+        # Poisson distribution doesn't have default lambda, checks if the input
+        # radio button (called dist) is for the Poisson distribution, and puts
+        # in lambda
         if(as.character(input$dist) == "pois") {
             vals <- NULL
             for(i in 1:input$numSamples) {
                 vals <- rbind(vals, rpois(input$sampleSize, lambda = 1))
             }
         }
+        # Other distributions have defaults
         else {
             vals <- NULL
             dist <- switch(input$dist, norm = rnorm, unif = runif, pois = rpois, exp = rexp, rnorm)
--- ui.R
+++ ui.R
@@ -1,15 +1,15 @@
 # Developing Data Products Shiny Project
 
-#' This shiny app demostrates the central limit theorem by allowing the user to 
-#' select the distribution type, sample size, and number of samples. It then 
-#' plots the corresponding histogram for the number of random draws or number of
-#' means. 
-#' 
-#' In ui.R, we set up sliders for sample size and number of samples, and radio
-#' buttons that will allow the user to choose the type of distribution. The 
-#' available distribution types are those that have been mentioned in the class, 
-#' normal, uniform, poisson, and exponential. This also includes a "submit"
-#' button because performing 10,000 draws takes a little time.
+# This shiny app demostrates the central limit theorem by allowing the user to 
+# select the distribution type, sample size, and number of samples. It then 
+# plots the corresponding histogram for the number of random draws or number of
+# means. 
+# 
+# In ui.R, we set up sliders for sample size and number of samples, and radio
+# buttons that will allow the user to choose the type of distribution. The 
+# available distribution types are those that have been mentioned in the class, 
+# normal, uniform, poisson, and exponential. This also includes a "submit"
+# button because performing 10,000 draws takes a little time.
 
 library(shiny)
 
